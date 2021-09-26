291d16ac35e510956e2126b8c5baec89f7add31b
KÜBRA KÖKEN <kubra.koken@outlook.com>
2021-09-26 12:48:57 +0300

    spring boot project created

---
+++ src/test/java/com/example/DeveloperCafe/DeveloperCafeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.DeveloperCafe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeveloperCafeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
